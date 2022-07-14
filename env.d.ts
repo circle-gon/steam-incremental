ed8e34301024a3a3303ddbf223fb307878ef9dcc
circle-gon <drew01547@gmail.com>
2022-07-14 19:38:55 -0400

    2 files modified

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,15 @@
 /// <reference types="vite/client" />
 
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
+import { steam } from './data/steam';
+
+declare global {
+  interface Window {
+    steam: typeof steam;
+  }
+}
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>;
   export default component;
